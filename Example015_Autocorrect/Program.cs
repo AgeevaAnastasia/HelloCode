8d2574d19a8453e58025326209ec387e60b90a0f
Anastasia Ageeva <anastasia.o.ageeva@gmail.com>
2022-07-20 17:52:39 +0300

    Add Example 015 Autocorrect

---
+++ Example015_Autocorrect/Program.cs
@@ -0,0 +1,39 @@
+﻿// Дан текст. Пробелы нужно заменить черточками, маленькие "к" - большими "К", большие "С" - маленькими "с"
+
+Console.WriteLine("Введите текст для автозамены символов.");
+string text = Console.ReadLine();
+Console.WriteLine();
+// string text = "Я думаю, - сказал князь, улыбаясь, - что, "
+//             + "ежели бы вас послали вместо нашего милого Винценгероде, "
+//             + "вы бы взяли приступом согласие прусского короля. "
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = qwerty
+//            012345  
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + text[i];
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '_');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+Console.WriteLine();
