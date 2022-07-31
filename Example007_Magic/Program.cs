9b7b62d325e34c393532ca017c9de2e3a3be2695
Anastasia Ageeva <anastasia.o.ageeva@gmail.com>
2022-07-31 18:14:34 +0300

    Add Game

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,43 +2,92 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 40, ya = 1,
-    xb = 1, yb = 20,
-    xc = 80, yc = 20;
+int x1 = 40, y1 = 1,
+    x2 = 1, y2 = 20,
+    x3 = 80, y3 = 20;
 
-Console.SetCursorPosition(xa, ya);
+Console.SetCursorPosition(x1, y1);
 Console.WriteLine("+");
 
-Console.SetCursorPosition(xb, yb);
+Console.SetCursorPosition(x2, y2);
 Console.WriteLine("+");
 
-Console.SetCursorPosition(xc, yc);
+Console.SetCursorPosition(x3, y3);
 Console.WriteLine("+");
 
-int x = xa, y = yc;
+int x = x1, y = y1;
 
 int count = 0;
 
 while(count < 10000)
 {
-    int what = new Random().Next(0, 3); //[0,3) 0, 1, 2
+    int what = new Random().Next(0, 3); //=[0,3) 0, 1, 2
     if(what == 0)
     {
-        x = (x + xa) / 2;
-        y = (y + ya) / 2;
+        x = (x + x1) / 2;
+        y = (y + y1) / 2;
     }
 
     if(what == 1)
     {
-        x = (x + xb) / 2;
-        y = (y + yb) / 2;
+        x = (x + x2) / 2;
+        y = (y + y2) / 2;
     }
      if(what == 2)
     {
-        x = (x + xc) / 2;
-        y = (y + yc) / 2;
+        x = (x + x3) / 2;
+        y = (y + y3) / 2;
     }
     Console.SetCursorPosition(x , y);
     Console.WriteLine("+");
     count = count + 1; // либо count+=1 либо count++
 }
+
+// Console.WriteLine();
+// Console.WriteLine();
+// Console.WriteLine();
+
+// int xa = 1, ya = 1,
+//     xb = 1, yb = 40,
+//     xc = 40, yc = 40,
+//     xd = 40, yd = 1;
+
+// Console.SetCursorPosition(xa, ya);
+// Console.WriteLine("+");
+
+// Console.SetCursorPosition(xb, yb);
+// Console.WriteLine("+");
+
+// Console.SetCursorPosition(xc, yc);
+// Console.WriteLine("+");
+
+// Console.SetCursorPosition(xd, yd);
+// Console.WriteLine("+");
+
+// int x = xa, y = yc;
+
+// int count = 0;
+
+// while(count < 10000)
+// {
+//     int what = new Random().Next(0, 3); //[0,3) 0, 1, 2
+//     if(what == 0)
+//     {
+//         x = (x + xa) / 2;
+//         y = (y + ya) / 2;
+//     }
+
+//     if(what == 1)
+//     {
+//         x = (x + xb) / 2;
+//         y = (y + yb) / 2;
+//     }
+//      if(what == 2)
+//     {
+//         x = (x + xc) / 2;
+//         y = (y + yc) / 2;
+//     }
+//     Console.SetCursorPosition(x , y);
+//     Console.WriteLine("+");
+//     count = count + 1; // либо count+=1 либо count++
+// }
---
+++ Example017_Game/Program.cs
@@ -0,0 +1,173 @@
+﻿Console.Clear();
+Console.WriteLine("Выберите: камень, ножницы или бумага?");
+string userText = Console.ReadLine();
+string user = String.Empty;
+
+string Enter(string userText1)
+{
+    if (userText1 == "к" || userText == "камень")
+    {
+        return "камень";
+    }
+    else if (userText1 == "н" || userText == "ножницы")
+    {
+        return "ножницы";
+    }
+    else if (userText1 == "б" || userText == "бумага")
+    {
+        return "бумага";    
+    }
+    else 
+    {
+        Console.WriteLine("Вы ввели неверное значение!");
+        throw new ArgumentException("Вы ввели неверное значение");
+    }
+}
+
+user = Enter(userText);
+int comp = new Random().Next(0, 3);
+int count = 0;
+string compText = String.Empty;
+
+string CompText(int comp1)
+{
+    if (comp1 == 0)
+    {
+        compText = "камень";
+    }
+    else if (comp1 == 1)
+    {
+        compText = "ножницы";
+    }
+    else 
+    {
+        compText = "бумага";
+    }
+    return compText; 
+}
+
+int Win(string user1)
+{
+    if (user1 == "камень")
+    {
+        if (comp == 1)
+        {
+            count = count - 1;
+        }
+        if (comp == 2)
+        {
+            count = count + 1;
+        }
+    }
+
+    else if (user1 == "ножницы")
+    {
+        if (comp == 2)
+        {
+           count = count - 1;
+        }
+        if (comp == 0)
+        {
+            count = count + 1;
+        }
+    }
+
+    else
+    {
+        if (comp == 0)
+        {
+            count = count - 1;
+        }
+        if (comp == 1)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+void SetResult(int count1)
+{
+    if (count1 < 0)
+    {
+        Console.WriteLine("-------------------------------------------------------------------");
+        Console.WriteLine($"ПОЗДРАВЛЯЕМ! ВЫ ВЫИГРАЛИ! Вы выбрали {user}, а компьютер - {compText}.");
+        Console.WriteLine("-------------------------------------------------------------------");
+    }
+    else if (count1 > 0)
+    {
+        Console.WriteLine("------------------------------------------------------------");
+        Console.WriteLine($"Увы, вы проиграли! Вы выбрали {user}, а компьютер - {compText}.");
+        Console.WriteLine("------------------------------------------------------------");
+    }
+    else 
+    {
+        Console.WriteLine("------------------------------------------------------");
+        Console.WriteLine($"У вас ничья! Вы выбрали {user}, а компьютер - {compText}.");
+        Console.WriteLine("------------------------------------------------------");
+    }
+}
+
+
+int win = Win(user);
+string compText1 = CompText(comp);
+SetResult(count);
+int gameCount = count;
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Второй тур! Выберите: камень, ножницы или бумага?");
+userText = Console.ReadLine();
+user = Enter(userText);
+comp = new Random().Next(0, 3);
+count = 0;
+compText = String.Empty;
+
+int win1 = Win(user);
+string compText11 = CompText(comp);
+SetResult(count);
+gameCount = gameCount + count;
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Третий тур, финальный! Выберите: камень, ножницы или бумага?");
+userText = Console.ReadLine();
+user = Enter(userText);
+comp = new Random().Next(0, 3);
+count = 0;
+compText = String.Empty;
+
+
+int win2 = Win(user);
+string compText12 = CompText(comp);
+SetResult(count);
+gameCount = gameCount + count;
+
+Console.WriteLine();
+Console.WriteLine();
+void GameResult(int gameCount1)
+{
+    if (gameCount < 0)
+    {
+        Console.WriteLine("||=================================================||");
+        Console.WriteLine("||ВЫ ПОБЕДИЛИ В ИГРЕ ПОСЛЕ ТРЁХ ТУРОВ. ПОЗДРАВЛЯЕМ!||");
+        Console.WriteLine("||=================================================||");
+    }
+    else if (gameCount > 0)
+    {
+        Console.WriteLine("||=======================================||");
+        Console.WriteLine("||Вы програли в игре после трёх туров ;-(||");
+        Console.WriteLine("||=======================================||");
+    }
+    else
+    {
+        Console.WriteLine("||======================================================||");
+        Console.WriteLine("||После трёх туров у вас ничья. Можно сыграть еще раз ;)||"); 
+        Console.WriteLine("||======================================================||");
+    }
+}
+
+GameResult(gameCount);
+
+
+
